78f0933b9eab807d2b9f52e01d7b5df39e57d483
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-10-29 21:08:22 -0400

    pwn-shell

--- shells/pwn.nix
+++ shells/pwn.nix
@@ -4,19 +4,33 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     xonsh
     python312
-    python312Packages.virtualenv  # Add venv to create Python virtual environments
+    python312Packages.virtualenv
     python312Packages.netifaces
 
+    # binex
     pwntools
     gdb
     gef
     pwndbg
     ropgadget
+
+    # cno
+    wireshark
+    gobuster
+    nmap
+    netcat-openbsd
+    thc-hydra
+    john
+    sqlmap
+    ffuf
+    aircrack-ng
+    metasploit
+    social-engineer-toolkit
+
   ];
 
   shellHook = ''
-    # Create a virtual environment in your home directory if it doesn't exist
-    if [ ! -d "$HOME/.venv/xonsh" ]; then
+    if [ ! -d "$HOME/.venv/pwn" ]; then
       python3 -m venv $HOME/.venv/xonsh
       source $HOME/.venv/xonsh/bin/activate
       pip install xontrib-vox
--- shells/xonsh.nix
+++ shells/xonsh.nix
@@ -4,12 +4,11 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     xonsh
     python312
-    python312Packages.virtualenv  # Add venv to create Python virtual environments
+    python312Packages.virtualenv
     python312Packages.netifaces
   ];
 
   shellHook = ''
-    # Create a virtual environment in your home directory if it doesn't exist
     if [ ! -d "$HOME/.venv/xonsh" ]; then
       python3 -m venv $HOME/.venv/xonsh
       source $HOME/.venv/xonsh/bin/activate
