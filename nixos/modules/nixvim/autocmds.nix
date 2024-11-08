1b0c7165051e6bf8e95228025013ef7392cd63e5
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-11-07 19:41:05 -0500

    added TODO & NOTE highlighting to nixvim

--- nixos/modules/nixvim/autocmds.nix
+++ nixos/modules/nixvim/autocmds.nix
@@ -26,6 +26,27 @@
         pattern = [ "*.asm" ];
         command = "0r ~/development/templates/template.asm";
       }
+
+      {
+        event = "Syntax";
+        pattern = "*";
+        command = "highlight CustomTODO guifg=#ff8800 gui=bold";
+      }
+      {
+        event = "Syntax";
+        pattern = "*";
+        command = "highlight CustomNOTE guifg=#00ff88 gui=italic";
+      }
+      {
+        event = "BufReadPost";
+        pattern = "*";
+        command = "syntax match CustomTODO /\\<TODO:/";
+      }
+      {
+        event = "BufReadPost";
+        pattern = "*";
+        command = "syntax match CustomNOTE /\\<NOTE:/";
+      }
     ];
   };
 }
