874666dec63c0f57f5763d4ed18f22286555cd6a
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2024-12-22 23:36:49 -0500

    msg

--- home-manager/modules/all.nix
+++ home-manager/modules/all.nix
@@ -1,14 +1,15 @@
 {
   imports = [
     ./bash.nix
-    ./catppuccin.nix
+    # ./catppuccin.nix
     ./git.nix
-    ./gtk.nix
-    ./hyprland.nix
+    # ./gtk.nix
+    # ./hyprland.nix
     ./i3.nix
     ./i3blocks.nix
     ./kitty.nix
     ./picom.nix
+    ./rofi.nix
     ./tmux.nix
     ./wofi.nix
     ./waybar.nix
--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -1,15 +1,32 @@
 {
   gtk = {
     enable = true;
-    catppuccin.accent = "sky";
-    catppuccin.flavor = "mocha";
-    catppuccin.size = "standard";
-    catppuccin.tweaks = "black";
 
-    catppuccin.icon = {
-      enable = true;
-      accent = "mauve";
-      flavor = "mocha";
+    theme = "Adapta-Nokto"; # Set the GTK theme name
+    iconTheme = "Papirus-Dark"; # Set the GTK icon theme name
+  };
+
+  # Cursor settings, separate from GTK
+  xsession = {
+    cursor = {
+      theme = "GoogleDot-White";
+      size = 24;
     };
   };
 }
+
+# {
+#   gtk = {
+#     enable = true;
+#     catppuccin.accent = "sky";
+#     catppuccin.flavor = "mocha";
+#     catppuccin.size = "standard";
+#     catppuccin.tweaks = "black";
+#
+#     catppuccin.icon = {
+#       enable = true;
+#       accent = "mauve";
+#       flavor = "mocha";
+#     };
+#   };
+# }
